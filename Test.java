722698c66c46674abefbbfcda427ef662f4b16da
nave <navekat22@gmail.com>
2017-10-02 03:42:02 +0000

    created test file

---
+++ Test.java
@@ -0,0 +1,10 @@
+
+public class Helloworld{
+
+      public static void main(String[] args){
+      	 System.out.println("before main");
+          String x = "abcd";
+          System.out.println(StringUtils.capitalize(x));
+          System.out.println("after main");
+      }
+}
