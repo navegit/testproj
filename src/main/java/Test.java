890feb9a8d288cd53401462b6ad36b9139025868
Vangeti, Naveen (Contingent Worker) <NRVangeti@comerica.com>
2018-10-21 23:36:34 -0400

    updated test

--- src/main/java/Test.java
+++ src/main/java/Test.java
@@ -4,7 +4,6 @@ public class Helloworld{
       public static void main(String[] args){
       	 System.out.println("before main");
           String x = "abcd";
-          System.out.println(StringUtils.capitalize(x));
           System.out.println("after main");
       }
 }
